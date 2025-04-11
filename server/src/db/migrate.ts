0a735110264ffef22080b3c571d14c7600093fff
Cepuh <fuloid@realzzy.xyz>
2025-04-11 22:29:13 +0700

    Migration sucks fr

--- server/src/db/migrate.ts
+++ server/src/db/migrate.ts
@@ -8,6 +8,20 @@ async function main() {
     const db: NodePgDatabase = drizzle(pool);
 
     logger.info('[Migration] Starting database migration...');
+    
+    // Check if migrations are already applied
+    const { rowCount } = await pool.query(`
+        SELECT EXISTS (
+            SELECT 1 FROM pg_tables 
+        );
+    `);
+    
+    if (rowCount && rowCount > 0) {
+        logger.info('[Migration] Found existing migrations. Skipped, bye!');
+        await pool.end();
+        return;
+    }
+
     await migrate(db, { migrationsFolder: './drizzle' });
 
     logger.info('[Migration] Database migration completed successfully.');
