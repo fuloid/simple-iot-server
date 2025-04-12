98372423fc03b3f625bc00e1c5bed45a99df4e40
Cepuh <fuloid@realzzy.xyz>
2025-04-12 23:17:36 +0700

    Update logger.ts

--- server/src/utils/logger.ts
+++ server/src/utils/logger.ts
@@ -4,7 +4,7 @@ import pretty from 'pino-pretty';
 const logger = pino(pretty());
 
 // make logger log info set debug min level
-logger.level = process.env.DEBUG ? 'debug' : 'info';
+logger.level = (process.env.DEBUG && Boolean(process.env.DEBUG)) ? 'debug' : 'info';
 logger.debug('Logger initialized with level:', logger.level);
 
 export default logger;
