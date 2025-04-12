30b20eeea1f68ca0c07129e2a702d8318075c9ec
Cepuh <fuloid@realzzy.xyz>
2025-04-12 10:05:27 +0700

    Update mqtt.ts

--- server/src/controllers/mqtt.ts
+++ server/src/controllers/mqtt.ts
@@ -37,7 +37,7 @@ app.post('/auth', async (c) => {
             logger.info('[MQTT] System authentication successful.');
             return c.json({
                 result: 'allow',
-                // expire_at: Date.now() + 24 * 60 * 60 * 1000,
+                expire_at: Date.now() + 24 * 60 * 60 * 1000,
                 acl: [
                     { permission: 'allow', action: 'all', topic: `device/#/ping` },
                     { permission: 'allow', action: 'all', topic: `device/#/data` },
