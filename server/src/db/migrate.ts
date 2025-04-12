5bcc594a06dae047bade2d616bb49a252b5bb94e
Cepuh <fuloid@realzzy.xyz>
2025-04-12 11:48:50 +0700

    Update migrate.ts

--- server/src/db/migrate.ts
+++ server/src/db/migrate.ts
@@ -3,29 +3,37 @@ import { migrate } from 'drizzle-orm/node-postgres/migrator';
 import { NodePgDatabase, drizzle } from 'drizzle-orm/node-postgres';
 import logger from '@/utils/logger';
 
+let restart = false;
+
 async function main() {
     const pool = new Pool({ connectionString: process.env.DATABASE_URL });
     const db: NodePgDatabase = drizzle(pool);
 
-    logger.info('[Migration] Starting database migration...');
+    logger.info(`[Migration] ${restart ? 'Res' : 'S'}tarting database migration...`);
     
-    // Check if migrations are already applied
-    const { rowCount } = await pool.query(`
-        SELECT EXISTS (
-            SELECT 1 FROM pg_tables 
-        );
-    `);
+    try {
+        // Check if migrations are already applied
+        const { rowCount } = await pool.query(`
+            SELECT EXISTS (
+                SELECT 1 FROM pg_tables 
+            );
+        `);
+
+        if (rowCount && rowCount > 0) {
+            logger.info('[Migration] Found existing migrations. Skipped, bye!');
+            await pool.end();
+            return;
+        }
     
-    if (rowCount && rowCount > 0) {
-        logger.info('[Migration] Found existing migrations. Skipped, bye!');
+        await migrate(db, { migrationsFolder: './drizzle' });
+    
+        logger.info('[Migration] Database migration completed successfully.');
         await pool.end();
-        return;
+    } catch (error) {
+        logger.error('[Migration] Checks error. database might be booting up, retries in 5 seconds...');
+        await new Promise(resolve => setTimeout(resolve, 5000));
+        main(); restart = true; return;
     }
-
-    await migrate(db, { migrationsFolder: './drizzle' });
-
-    logger.info('[Migration] Database migration completed successfully.');
-    await pool.end();
 }
 
 main();
